f4c215bc9ae8e1cdba6b715e87ab177033b48f79
Vinicius Guilherme de Freitas Andrade <vinicius.guilherme225@gmail.com>
2024-06-05 01:12:42 -0300

    correções docker

--- database/init-db.sh
+++ database/init-db.sh
@@ -2,4 +2,4 @@
 sleep 20s
 
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "MinhaSenhaForte123!" -Q "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'Schedule') CREATE DATABASE Schedule;"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -Q "IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'Schedule') CREATE DATABASE Schedule;"
